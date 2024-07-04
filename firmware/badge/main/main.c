0b376dd993ba627a9efd9139fcfdb3883f25955c
Kevin Jahaziel Leon Morales <jahaziel.lem@gmail.com>
2024-07-04 15:51:56 -0600

    feat: CTF BLE

--- firmware/badge/main/main.c
+++ firmware/badge/main/main.c
@@ -2,6 +2,7 @@
 #include "ajo_module.h"
 #include "cat_console.h"
 #include "catdos_module.h"
+#include "ctf_ble.h"
 #include "esp_event.h"
 #include "esp_log.h"
 #include "esp_timer.h"
@@ -46,4 +47,7 @@ void app_main(void) {
     menu_screens_display_menu();
     preferences_put_int("MENUNUMBER", 99);
   }
+  leds_off();
+
+  // ctf_ble_module_begin();
 }
