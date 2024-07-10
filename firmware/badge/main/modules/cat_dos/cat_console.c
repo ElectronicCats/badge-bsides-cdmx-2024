70df1f91b92793763f0792782b3e94f21bb40647
deimos <deimoshall@gmail.com>
2024-07-10 09:47:02 -0600

    refactor: replace uart to jtag config for esp console

--- firmware/badge/main/modules/cat_dos/cat_console.c
+++ firmware/badge/main/modules/cat_dos/cat_console.c
@@ -13,8 +13,8 @@
 #include "argtable3/argtable3.h"
 #include "cmd_catdos.h"
 #include "cmd_wifi.h"
-#include "driver/uart.h"
-#include "driver/uart_vfs.h"
+// #include "driver/uart.h"
+// #include "driver/uart_vfs.h"
 #include "esp_console.h"
 #include "esp_log.h"
 #include "esp_system.h"
@@ -48,61 +48,75 @@ static void initialize_console(void) {
   setvbuf(stdin, NULL, _IONBF, 0);
 
   /* Minicom, screen, idf_monitor send CR when ENTER key is pressed */
-  uart_vfs_dev_port_set_rx_line_endings(CONFIG_ESP_CONSOLE_UART_NUM,
-                                        ESP_LINE_ENDINGS_CR);
+  // uart_vfs_dev_port_set_rx_line_endings(CONFIG_ESP_CONSOLE_UART_NUM,
+  //                                       ESP_LINE_ENDINGS_CR);
   /* Move the caret to the beginning of the next line on '\n' */
-  uart_vfs_dev_port_set_tx_line_endings(CONFIG_ESP_CONSOLE_UART_NUM,
-                                        ESP_LINE_ENDINGS_CRLF);
+  // uart_vfs_dev_port_set_tx_line_endings(CONFIG_ESP_CONSOLE_UART_NUM,
+  //                                       ESP_LINE_ENDINGS_CRLF);
 
   /* Configure UART. Note that REF_TICK is used so that the baud rate remains
    * correct while APB frequency is changing in light sleep mode.
    */
-  const uart_config_t uart_config = {
-      .baud_rate = CONFIG_ESP_CONSOLE_UART_BAUDRATE,
-      .data_bits = UART_DATA_8_BITS,
-      .parity = UART_PARITY_DISABLE,
-      .stop_bits = UART_STOP_BITS_1,
-#if SOC_UART_SUPPORT_REF_TICK
-      .source_clk = UART_SCLK_REF_TICK,
-#elif SOC_UART_SUPPORT_XTAL_CLK
-      .source_clk = UART_SCLK_XTAL,
-#endif
-  };
-  /* Install UART driver for interrupt-driven reads and writes */
-  ESP_ERROR_CHECK(
-      uart_driver_install(CONFIG_ESP_CONSOLE_UART_NUM, 256, 0, 0, NULL, 0));
-  ESP_ERROR_CHECK(uart_param_config(CONFIG_ESP_CONSOLE_UART_NUM, &uart_config));
+  //   const uart_config_t uart_config = {
+  //       .baud_rate = CONFIG_ESP_CONSOLE_UART_BAUDRATE,
+  //       .data_bits = UART_DATA_8_BITS,
+  //       .parity = UART_PARITY_DISABLE,
+  //       .stop_bits = UART_STOP_BITS_1,
+  // #if SOC_UART_SUPPORT_REF_TICK
+  //       .source_clk = UART_SCLK_REF_TICK,
+  // #elif SOC_UART_SUPPORT_XTAL_CLK
+  //       .source_clk = UART_SCLK_XTAL,
+  // #endif
+  //   };
+  //   /* Install UART driver for interrupt-driven reads and writes */
+  //   ESP_ERROR_CHECK(
+  //       uart_driver_install(CONFIG_ESP_CONSOLE_UART_NUM, 256, 0, 0, NULL,
+  //       0));
+  //   ESP_ERROR_CHECK(uart_param_config(CONFIG_ESP_CONSOLE_UART_NUM,
+  //   &uart_config));
+
+  /*--- Initialize Console ---*/
+  esp_console_repl_t* repl = NULL;
+  esp_console_repl_config_t repl_config = ESP_CONSOLE_REPL_CONFIG_DEFAULT();
+  // initialize_filesystem();
+  // repl_config.history_save_path = HISTORY_FILE_PATH;
+  repl_config.prompt = "bsides>";
+
+  esp_console_dev_usb_serial_jtag_config_t usbjtag_config =
+      ESP_CONSOLE_DEV_USB_SERIAL_JTAG_CONFIG_DEFAULT();
+  ESP_ERROR_CHECK(esp_console_new_repl_usb_serial_jtag(&usbjtag_config,
+                                                       &repl_config, &repl));
 
   /* Tell VFS to use UART driver */
-  uart_vfs_dev_use_driver(CONFIG_ESP_CONSOLE_UART_NUM);
+  // uart_vfs_dev_use_driver(CONFIG_ESP_CONSOLE_UART_NUM);
 
   /* Initialize the console */
-  esp_console_config_t console_config = {.max_cmdline_args = 8,
-                                         .max_cmdline_length = 256,
-#if CONFIG_LOG_COLORS
-                                         .hint_color = atoi(LOG_COLOR_CYAN)
-#endif
-  };
-  ESP_ERROR_CHECK(esp_console_init(&console_config));
+  //   esp_console_config_t console_config = {.max_cmdline_args = 8,
+  //                                          .max_cmdline_length = 256,
+  // #if CONFIG_LOG_COLORS
+  //                                          .hint_color = atoi(LOG_COLOR_CYAN)
+  // #endif
+  //   };
+  // ESP_ERROR_CHECK(esp_console_init(&console_config));
 
   /* Configure linenoise line completion library */
   /* Enable multiline editing. If not set, long commands will scroll within
    * single line.
    */
-  linenoiseSetMultiLine(1);
+  // linenoiseSetMultiLine(1);
 
-  /* Tell linenoise where to get command completions and hints */
-  linenoiseSetCompletionCallback(&esp_console_get_completion);
-  linenoiseSetHintsCallback((linenoiseHintsCallback*) &esp_console_get_hint);
+  // /* Tell linenoise where to get command completions and hints */
+  // linenoiseSetCompletionCallback(&esp_console_get_completion);
+  // linenoiseSetHintsCallback((linenoiseHintsCallback*) &esp_console_get_hint);
 
-  /* Set command history size */
-  linenoiseHistorySetMaxLen(100);
+  // /* Set command history size */
+  // linenoiseHistorySetMaxLen(100);
 
-  /* Set command maximum length */
-  linenoiseSetMaxLineLen(console_config.max_cmdline_length);
+  // /* Set command maximum length */
+  // linenoiseSetMaxLineLen(console_config.max_cmdline_length);
 
-  /* Don't return empty lines */
-  linenoiseAllowEmpty(false);
+  // /* Don't return empty lines */
+  // linenoiseAllowEmpty(false);
 }
 
 void cat_console_begin() {
