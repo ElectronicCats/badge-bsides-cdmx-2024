d742bf6a286961ed8cdf867182c9c92f70244211
deimos <deimoshall@gmail.com>
2024-07-11 14:17:30 -0600

    feat: compress build files with get build script

--- firmware/badge/get_build.sh
+++ firmware/badge/get_build.sh
@@ -2,6 +2,8 @@
 
 # This script is used to get the build files needed to flash the badge using the esptool.py script.
 
+PROJECT_NAME="badge"
+
 # Build the firmware
 make compile
 
@@ -11,7 +13,13 @@ mkdir -p build_files/bootloader/
 
 # Copy the firmware files to the build directory
 echo "Copying firmware files to build directory..."
-cp build/badge.bin build_files/
+cp build/$PROJECT_NAME.bin build_files/
 cp build/partition_table/partition-table.bin build_files/partition_table/
 cp build/bootloader/bootloader.bin build_files/bootloader/
+
+# Compress build_files and delete directory
+echo "Compressing build_files..."
+tar -czvf build_files.tgz build_files
+rm -rf build_files/
+
 echo "Done!"
