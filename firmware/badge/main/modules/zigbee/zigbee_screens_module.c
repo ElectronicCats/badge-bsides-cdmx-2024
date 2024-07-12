a0d54730a2e21dece21a6ddef1f9791152d028c6
deimos <deimoshall@gmail.com>
2024-07-12 09:49:04 -0600

    refactor: zb switch labels

--- firmware/badge/main/modules/zigbee/zigbee_screens_module.c
+++ firmware/badge/main/modules/zigbee/zigbee_screens_module.c
@@ -15,40 +15,40 @@ void zigbee_screens_module_toggle_released() {
 
 void zigbee_screens_module_creating_network() {
   oled_screen_clear();
-  oled_screen_display_text("Creating", 25, 3, OLED_DISPLAY_NORMAL);
-  oled_screen_display_text("network...", 20, 4, OLED_DISPLAY_NORMAL);
+  oled_screen_display_text("Creating", 25, 1, OLED_DISPLAY_NORMAL);
+  oled_screen_display_text("network...", 20, 2, OLED_DISPLAY_NORMAL);
 }
 
 void zigbee_screens_module_creating_network_failed() {
   oled_screen_clear();
-  oled_screen_display_text("Creating", 25, 3, OLED_DISPLAY_NORMAL);
-  oled_screen_display_text("network", 29, 4, OLED_DISPLAY_NORMAL);
-  oled_screen_display_text("failed", 33, 5, OLED_DISPLAY_NORMAL);
+  oled_screen_display_text("Creating", 25, 0, OLED_DISPLAY_NORMAL);
+  oled_screen_display_text("network", 29, 1, OLED_DISPLAY_NORMAL);
+  oled_screen_display_text("failed", 33, 2, OLED_DISPLAY_NORMAL);
 }
 
 void zigbee_screens_module_waiting_for_devices() {
   static uint8_t dots = 0;
   dots = ++dots > 3 ? 0 : dots;
   oled_screen_clear_line(80, 4, OLED_DISPLAY_NORMAL);
-  oled_screen_display_text("Waiting for", 19, 3, OLED_DISPLAY_NORMAL);
-  oled_screen_display_text("devices", 24, 4, OLED_DISPLAY_NORMAL);
+  oled_screen_display_text("Waiting for", 19, 1, OLED_DISPLAY_NORMAL);
+  oled_screen_display_text("devices", 24, 2, OLED_DISPLAY_NORMAL);
   // Print dots from lef to right
   for (int i = 0; i < dots; i++) {
-    oled_screen_display_text(".", 80 + (i * 8), 4, OLED_DISPLAY_NORMAL);
+    oled_screen_display_text(".", 80 + (i * 8), 2, OLED_DISPLAY_NORMAL);
   }
 }
 
 void zigbee_screens_module_no_devices_found() {
   oled_screen_clear();
   vTaskDelay(100 / portTICK_PERIOD_MS);
-  oled_screen_display_text("No devices", 24, 3, OLED_DISPLAY_NORMAL);
-  oled_screen_display_text("found", 44, 4, OLED_DISPLAY_NORMAL);
+  oled_screen_display_text("No devices", 24, 1, OLED_DISPLAY_NORMAL);
+  oled_screen_display_text("found", 44, 2, OLED_DISPLAY_NORMAL);
 }
 
 void zigbee_screens_module_closing_network() {
   oled_screen_clear();
-  oled_screen_display_text("Closing", 25, 3, OLED_DISPLAY_NORMAL);
-  oled_screen_display_text("network...", 20, 4, OLED_DISPLAY_NORMAL);
+  oled_screen_display_text("Closing", 25, 1, OLED_DISPLAY_NORMAL);
+  oled_screen_display_text("network...", 20, 2, OLED_DISPLAY_NORMAL);
 }
 
 void zigbee_screens_module_display_status(uint8_t status) {
